271e83afd45b2c31e6df23151ffdf3381e1136a5
jieme8@gmail.com <jieme8@gmail.com>
2015-12-04 09:29:28 +0800

    modified

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ chrome.tabs.getSelected(function(t){
 	console.log(t)
 	$("#demo").qrcode({ 
 	    //render: "table", //table方式 
-	    width: 400, //宽度 
-	    height:400, //高度 
+	    width: 200, //宽度 
+	    height:200, //高度 
 	    text: t.url//任意内容 
 	}); 
 })
